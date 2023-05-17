0fd5d1e29ce0d29f3962fdd7bbd306f1fb196156
Thomas Peek <peekthomas369@icloud.com>
2023-05-17 14:27:59 -0600

    trailer model updated in backend

--- hitchtrailernow.client/src/models/Trailer.js
+++ hitchtrailernow.client/src/models/Trailer.js
@@ -3,22 +3,28 @@ import { TrailerRenter } from "./TrailerRenter.js"
  export class Trailer extends TrailerRenter{
     constructor(data){
         this.id = data.id
-        this.accountId = data.accountId
         this.archived = data.archived
+        this.title = data.title
+        this.year = data.year
+        this.make = data.make
+        this.capacity = data.capacity
+        this.lengthSize = data.lengthSize
+        this.width = data.width
         this.description = data.description
         this.details = data.details
-        this.features = data.features
-        this.capacity = data.capacity
-        this.title = data.title
-        this.type = data.type
+        this.rules = data.rules
+        this.cost = data.cost
         this.addons = data.addons
         this.deposit = data.deposit
-        this.cost = data.cost
+        this.type = data.type
         this.img1 = data.img1
         this.img2 = data.img2
         this.img3 = data.img3
         this.img4 = data.img4
         this.img5 = data.img5
         this.img6 = data.img6
+        this.img6 = data.img6
+        this.img7 = data.img7
+        this.accountId = data.accountId
     }
 }
