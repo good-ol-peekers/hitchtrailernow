85f21a5a604f794cddf180dc10e51ec05a4aed25
Thomas Peek <peekthomas369@icloud.com>
2023-05-25 09:41:42 -0600

    router.js updated trailersPage

--- hitchtrailernow.client/src/pages/TrailerArrayPage.vue
+++ hitchtrailernow.client/src/pages/TrailerArrayPage.vue
@@ -11,6 +11,15 @@ import { onMounted, computed, onUpdated } from 'vue';
 
 export default {
     setup(){
+
+        async function getAllTrailers() {
+            try {
+                await trailersS
+            } catch (error) {
+                Pop.error(error, "get All Trailers")
+            }
+        }
+
         OnMounted(() => {
             getAllTrailers();
         });
